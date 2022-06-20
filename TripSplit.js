a98ba5367e474ad0ccf70ee40f6916333ea57769
Kparkman5 <kyleparkman5@gmail.com>
2022-06-19 17:37:41 -0700

    Changed ids from 'user_owes' to 'Kyle_owes' or 'Name_owes'

--- TripSplit.js
+++ TripSplit.js
@@ -5,6 +5,9 @@ var user2Dict = {}
 var user3Dict = {}
 var user4Dict = {}
 
+
+var userDictArray = [{}, {}, {}, {}]
+
 const kyleObj = document.getElementsByClassName('user')[0]
 const amraObj = document.getElementsByClassName('user')[1]
 const chrisObj = document.getElementsByClassName('user')[2]
@@ -19,54 +22,49 @@ assignDictionaries(numUsers)
 
 
 
-
 groupChargeButton.addEventListener('click', function () {
 	var total = document.getElementById('group_charge_amount').value
-	groupOwes = total / 4
+	groupOwes = (total / 4)
 	chargeGroup(groupOwes)
-})
+	console.log(userDictArray)
 
+
+})
 
 function assignDictionaries(numUsers) {
 
-	user1Dict.user2 = {}
-	user1Dict.user3 = {}
-	user1Dict.user4 = {}
-
-	user1Dict.user2[name2] = 0
-	user1Dict.user3[name3] = 0
-	user1Dict.user4[name4] = 0
-
-	user2Dict.user1 = {}
-	user2Dict.user3 = {}
-	user2Dict.user4 = {}
-
-	user2Dict.user1[name1] = 0
-	user2Dict.user3[name3] = 0
-	user2Dict.user4[name4] = 0
-
-	user3Dict.user1 = {}
-	user3Dict.user2 = {}
-	user3Dict.user4 = {}
-
-	user3Dict.user1[name1] = 0
-	user3Dict.user2[name2] = 0
-	user3Dict.user4[name4] = 0
-
-	user4Dict.user1 = {}
-	user4Dict.user2 = {}
-	user4Dict.user3 = {}
-
-	user4Dict.user1[name1] = 0
-	user4Dict.user2[name2] = 0
-	user4Dict.user3[name3] = 0
+	for (var i = 0; i < numUsers; i++){
+		temp = document.getElementsByClassName('user')[i].textContent
+		for (var j = 0; j < numUsers; j++){
+			temp2 = document.getElementsByClassName('user')[j].textContent
+			if (temp2 != temp){
+				userDictArray[i][temp2] = 0
+ 			}
+		}
+	}
 }
 
 
 
+function chargeGroup(groupOwes){
+	for (var i = 1; i < numUsers; i++){
+		temp = document.getElementsByClassName('user')[0].textContent
+		userDictArray[i][temp] += groupOwes
+	}
+	for (var i = 1; i < numUsers; i++){
+		temp = document.getElementsByClassName('user')[i].textContent
+
+		for (var j = 0; j < numUsers - 1; j++) {
+			var userLi = document.getElementById(temp + '_owes').querySelectorAll('li')[j]
+			if (userLi.id.includes('Kyle')) {
+				userLi.innerText = 'Kyle' + ' $' + userDictArray[i].Kyle.toFixed(2)
+			}
+		}
+	}
+}
 
 
-function chargeGroup(amount) {
+/* function chargeGroup(amount) {
 
 	for (var i = 0; i < numUsers - 1; i++){
 		var userLi = document.getElementById('user2_owes').querySelectorAll('li')[i]
@@ -74,14 +72,14 @@ function chargeGroup(amount) {
 		console.log(userLi)
 		if (userLi.id.includes('Kyle')) {
 			user2Dict.user1['Kyle'] += amount
-			userLi.innerText = 'Kyle' + ' $' + user2Dict.user1['Kyle']
+			userLi.innerText = 'Kyle' + ' $' + user2Dict.user1['Kyle'].toFixed(2)
 			/*
 			user2Dict.user1[userName] += amount
 			userLi.value = user2Dict.user1[userName]
 			console.log(userLi.value)
 			userLi.innerText = userName + ' $' + user2Dict.user1[userName]
 			console.log(userLi.value)
-			*/
+			
 		}
 	}
 
@@ -90,7 +88,7 @@ function chargeGroup(amount) {
 		var userName = userLi.textContent
 		if (userLi.id.includes('Kyle')) {
 			user3Dict.user1['Kyle'] += amount
-			userLi.innerText = 'Kyle' + ' $' + user3Dict.user1['Kyle']
+			userLi.innerText = 'Kyle' + ' $' + user3Dict.user1['Kyle'].toFixed(2)
 		}
 	}
 
@@ -99,7 +97,7 @@ function chargeGroup(amount) {
 		var userName = userLi.textContent
 		if (userLi.id.includes('Kyle')) {
 			user4Dict.user1['Kyle'] += amount
-			userLi.innerText = 'Kyle' + ' $' + user4Dict.user1['Kyle']
+			userLi.innerText = 'Kyle' + ' $' + user4Dict.user1['Kyle'].toFixed(2)
 		}
 	}
 
@@ -109,7 +107,7 @@ function chargeGroup(amount) {
 	console.log('user4 ' + JSON.stringify(user4Dict))
 }
 
-
+*/
 /*
 	var Kyle = document.getElementById('owes_user1')
 	for (var i = 2; i < numUsers + 1; i++){
